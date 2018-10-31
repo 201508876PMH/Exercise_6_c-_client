1b8b684864b46503a6b0236bef8b5ec0bc9e06c3
root <olivermonberg@gmail.com>
2018-10-31 09:27:07 +0100

    faerdig

--- file_client/file_client.cs
+++ file_client/file_client.cs
@@ -9,54 +9,22 @@ namespace tcp
 {
 	class file_client
 	{
-		/// <summary>
-		/// The PORT.
-		/// </summary>
-		const int PORT = 9000;
-		/// <summary>
-		/// The BUFSIZE.
-		/// </summary>
-		const int BUFSIZE = 1000;
+        const int PORT = 9000;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="file_client"/> class.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments. First ip-adress of the server. Second the filename
-		/// </param>
 		private file_client (string[] args)
 		{
-			
             Socket ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            ClientSocket.Connect("10.192.155.179", 9000);
+			ClientSocket.Connect(args[0], PORT);
 		    Console.WriteLine("Connected...");
             NetworkStream ioClient = new NetworkStream(ClientSocket);
 
-		    while (true)
-		    {
-		        Console.WriteLine("Select the file you want to download from the server.");
-		        Console.Write("C:/");
-
-		        string fileToRecieve = "C:/" + Console.ReadLine();
-
-		        Console.WriteLine("Choose name, type and where to save the file.");
-		        Console.Write("C:/");
-
-		        string saveFilePath = "C:/" + Console.ReadLine();
-
-		        receiveFile(fileToRecieve, ioClient, saveFilePath);
-            }
+		    Console.WriteLine("Choose where to save the file, name and format.");
+		        
+            string saveFilePath = Console.ReadLine();
+            
+			receiveFile(args[1], ioClient, saveFilePath);
         }
 
-		/// <summary>
-		/// Receives the file.
-		/// </summary>
-		/// <param name='fileName'>
-		/// File name.
-		/// </param>
-		/// <param name='io'>
-		/// Network stream for reading from the server
-		/// </param>
 		private void receiveFile (String filePath, NetworkStream io, string saveFilePath)
 		{
 		    LIB.writeTextTCP(io, filePath);
@@ -77,8 +45,7 @@ namespace tcp
 
                 FileStream fs = File.Create(saveFilePath);
 
-		        int offset = 0, count = 1000;
-
+		        int offset = 0, count = 1000;            
 		        int lastRead = 1;
 
 		        while (lastRead >= 0)
@@ -98,52 +65,18 @@ namespace tcp
 		            {
 		                count = (int)fileSize;
 		                --lastRead;
-		                //fileSize -= 1000;
-		            }
-		            
-		            
-
-		            //offset += 1000;
-
-		            //if (offset < fileSize && (offset + 1000 > fileSize))
-		            //{
-		            //    count = (int)fileSize - offset;
-		            //}
-
+		            }               
 		        }
 
                 fs.Close();
-                
-
-                /*
-		        byte[] readBuf = new byte[fileSize];
-                
-		        int i = 0;
-		        while (fileSize != 0)
-		        {
-		            readBuf[i] = (byte) io.ReadByte();
-		            ++i;
-		            --fileSize;
-		        }
-
-		        File.WriteAllBytes(saveFilePath, readBuf);
-                */
 
 		        Console.WriteLine("File downloaded.\n");
 		    }
 		}
 
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments.
-		/// </param>
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Client starts...");
-			new file_client(args);
-		    System.Console.ReadKey();
+			file_client fc =  new file_client(args);
 		}
 	}
 }
